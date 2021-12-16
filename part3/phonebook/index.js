fb7a11748d6405f13097e9fdd4de8fa28a471367
Oraion13 <65845210+Oraion13@users.noreply.github.com>
2021-12-16 13:53:11 +0530

    Added Heroku, build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
+app.use(cors());
 
 morgan.token("data", (req, res) => JSON.stringify(req.body));
 
