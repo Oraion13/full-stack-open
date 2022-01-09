bbef20629a62c0603bf3ee0d3ad59d02efacb447
Oraion13 <65845210+Oraion13@users.noreply.github.com>
2022-01-09 09:40:28 +0530

    part 5 exercises: 5.5 - 5.10 completed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,14 +1,24 @@
-
 import Display from "./Display";
 
-const Blog = (props) => {
-    return (
-        <>
-        {props.blogs.map((blog) => (
-        <Display key={blog.id} title={blog.title} author={blog.author} />
+const Blog = ({ blogs, setBlogs, userName }) => {
+  return (
+    <>
+      {blogs.map((blog) => (
+        <Display
+          key={blog.id}
+          id={blog.id}
+          blogs={blogs}
+          setBlogs={setBlogs}
+          title={blog.title}
+          author={blog.author}
+          url={blog.url}
+          likes={blog.likes}
+          blogUser={blog.user[0]}
+          userName={userName}
+        />
       ))}
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default Blog;
